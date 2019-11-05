47378b12b60fa4180ac1b733aec732a8e69ef35d
Danny Lee <dreamer@panix.com>
2019-11-05 08:37:20 -0500

    complete, with object

--- src/UserClass.js
+++ src/UserClass.js
@@ -10,25 +10,30 @@ class User {
         this.phone = user.phone
         this.state = user.location.state
         this.cell = user.cell
-        // this.date_of_birth = user.dob.date    // => 1959-01-30T23:36:59.505Z
+        // this.date_of_birth = user.dob.date    
+        // ^---This version was no good because it looked like this => 1959-01-30T23:36:59.505Z
+        // So, split it at T and take first string out of the two in the returned by split()
         let datefix = user.dob.date.split("T")[0]
         this.date_of_birth = datefix
         this.postcode = user.location.postcode
+        this.profile_picture = user.picture.large
         User.all.push(this)
     }
 
-    // this is apparently a method and not a function.  I cannot add keyword  'function' 
-    // before 'setAttribute' or it stops working
+    // setAttribute() is apparently a method and not a function.  Adding the  keyword  'function' 
+    // before 'setAttribute' and it stops working.
 
     setAttribute(variable){
-
         var s = document.getElementById(variable);
-
-        // console.log(s)
-
-        // have to use [] instead of . notation or else "variable" is not read properly 
-
-        s.innerHTML = this[variable];   
+        // console.log(s) // check value of s to is we're picking up the right element 
+        // below, had used [] instead of . notation.  Dot notation does not read properly.  Also, had to add a special else for profile_picture
+        // because .src is a subattribute of the attribute :|
+        if (variable != "profile_picture") {
+            s.innerHTML = this[variable];   
+        }
+        else {
+            s.src = this[variable];
+        }
     }   
 }
 
